20137ec61104b788b87d6db88f2a52e3fd75a069
Aditya <adityasinghboss1234@gmail.com>
2023-12-14 03:14:28 +0530

    feat: Signaling Server Setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Saksham Webrtc server");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user is disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
