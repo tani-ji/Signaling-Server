ba4354a3c5bb6e0cdfb14bc90e1b7beaf126b5fd
Aditya <adityasinghboss1234@gmail.com>
2023-12-14 08:00:40 +0530

    remove disconnected users

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,15 +1,39 @@
 import { Socket } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 
+const rooms: Record<string, string[]> = {};
+
+interface IRoomParams {
+  roomId: string;
+  peerId: string;
+}
+
 export const roomHandler = (socket: Socket) => {
   const createRoom = () => {
     const roomId = uuidv4();
+    rooms[roomId] = [];
     socket.emit("room-created", { roomId });
     console.log(`user created room with id: ${roomId}`);
   };
-  const joinRoom = ({ roomId }: { roomId: string }) => {
-    console.log("user joined room", roomId);
-    socket.join(roomId);
+  const joinRoom = ({ roomId, peerId }: IRoomParams) => {
+    if (rooms[roomId]) {
+      console.log("user joined room", roomId, peerId);
+      rooms[roomId].push(peerId);
+      socket.join(roomId);
+      socket.emit("get-users", {
+        roomId,
+        participants: rooms[roomId],
+      });
+    }
+
+    socket.on("disconnect", () => {
+      console.log("user left the room", peerId);
+      leaveRoom({ roomId, peerId });
+    });
+  };
+  const leaveRoom = ({ roomId, peerId }: IRoomParams) => {
+    rooms[roomId] = rooms[roomId].filter((id) => id !== peerId);
+    socket.to(roomId).emit("user-disconnected", peerId);
   };
 
   socket.on("create-room", createRoom);
